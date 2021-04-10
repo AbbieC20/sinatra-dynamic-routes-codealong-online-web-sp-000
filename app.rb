631025dea3d3a10544119c7017aaf5db4a663772
remote-torrent-6747 <abbyc@sky.com>
2021-04-10 11:58:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,6 +13,10 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
-  # Code your final two routes here:
+  get "/goodbye/:name" do
+    @person_name = params[:name]
+    "Hello #{@person_name}!"
+  end
+
 
 end
