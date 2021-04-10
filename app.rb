3eb97a59ba37cee743d7dc1c3655a2beb3832ee6
remote-torrent-6747 <abbyc@sky.com>
2021-04-10 12:02:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:number1/:number2" do
-    @number_output = params[:number1 * :number2]
+    @number_output = params[:number1.to_i * :number2.to_i]
     "@number_output"
   end
 
